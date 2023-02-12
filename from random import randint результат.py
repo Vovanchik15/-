82ba0bfc7f9fd0c91b0fc4fa8b03880ecbdec31c
Vovanchik15 <123307331+Vovanchik15@users.noreply.github.com>
2023-02-12 19:19:13 +0300

    Add files via upload

---
+++ from random import randint результат.py
@@ -0,0 +1,54 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\KIDKOD_3\Desktop\Students\vova\from random import randint.py 
+Попробуй угадай число от 1 до 100
+Ваше предположение: 50
+Число меньше 50
+Ваше предположение: 30
+Число больше 30
+Ваше предположение: 40
+Число меньше 40
+Ваше предположение: 35
+Число меньше 35
+Ваше предположение: 33
+Число меньше 33
+Ваше предположение: 32
+Вы угадали верно!
+>>> 
+ RESTART: C:\Users\KIDKOD_3\Desktop\Students\vova\from random import randint.py 
+Traceback (most recent call last):
+  File "C:\Users\KIDKOD_3\Desktop\Students\vova\from random import randint.py", line 6, in <module>
+    file=open('game_result', 'w', enconding="utf8")
+TypeError: 'enconding' is an invalid keyword argument for open()
+>>> 
+ RESTART: C:\Users\KIDKOD_3\Desktop\Students\vova\from random import randint.py 
+Попробуй угадай число от 1 до 100
+Ваше предположение: 50
+Число больше 50
+Ваше предположение: 75
+Число меньше 75
+Ваше предположение: 60
+Число меньше 60
+Ваше предположение: 55
+Число меньше 55
+Ваше предположение: 53
+Число меньше 53
+Ваше предположение: 51
+Число больше 51
+Ваше предположение: 52
+Вы угадали верно!
+>>> 
+ RESTART: C:\Users\KIDKOD_3\Desktop\Students\vova\from random import randint.py 
+Попробуй угадай число от 1 до 100
+Ваше предположение: 50
+Число меньше 50
+Ваше предположение: 25
+Число больше 25
+Ваше предположение: 40
+Число больше 40
+Ваше предположение: 46
+Число меньше 46
+Ваше предположение: 43
+Вы угадали верно!
+>>> 
